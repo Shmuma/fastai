ea4ebeb8d8d3156ebe06585ea71088bfe80a9fb0
Max Lapan <max.lapan@gmail.com>
2018-09-12 19:20:45 +0200

    Parallel

--- my/inclusive-images/convert.py
+++ my/inclusive-images/convert.py
@@ -3,7 +3,7 @@
 
 # # Data conversion for training the model
 
-# In[25]:
+# In[32]:
 
 
 import os
@@ -11,6 +11,7 @@ import shutil
 import pandas as pd
 import PIL.Image
 from tqdm import tqdm
+import concurrent.futures as futures
 
 
 # In[20]:
@@ -34,7 +35,7 @@ print("Loading bounding box data...")
 df_bboxes = pd.read_csv(f'{DATA_PATH}train_bounding_boxes.csv')
 
 
-# In[26]:
+# In[42]:
 
 
 print("Cleaning up old data...")
@@ -44,39 +45,84 @@ if os.path.exists(TRAIN_PATH):
 os.makedirs(TRAIN_PATH)
 
 
-# In[27]:
+# In[36]:
 
 
+class Job:
+    def __init__(self, img_id):
+        self.img_id = img_id
+        self.sub_map = {}
+        self.sub_idx = 0
+        self.images_labels = []
+        
+    def add_row(self, row):
+        if self.img_id != row['ImageID']:
+            return False
+        label = row['LabelName']
+        bbox = [min(1.0, max(0, round(row[t], 5))) for t in ('XMin', 'YMin', 'XMax', 'YMax')]
+        bbox = tuple(bbox)
+        tgt_img_id = self.sub_map.get(bbox)
+        if tgt_img_id is None:
+            tgt_img_id = f'{self.img_id}_{self.sub_idx:05}'
+            self.sub_idx += 1
+            self.sub_map[bbox] = tgt_img_id
+        self.images_labels.append((tgt_img_id, label))
+        return True
+
+    def add_labels(self, df):
+        for img, label in self.images_labels:
+            df = df.append({'ImageID': img, 'LabelName': label}, ignore_index=True)
+        return df
+    
+    def submit(self, executor):
+        return executor.submit(do_job, self.img_id, self.sub_map)
+        
+def do_job(img_id, sub_map):
+    fname = f'{DATA_PATH}train/{img_id}.jpg'
+    img = PIL.Image.open(fname)
+    w, h = img.size
+    for bbox, tgt_img_id in sub_map.items():
+        crop = (w * bbox[0], h * bbox[1], w * bbox[2], h * bbox[3])
+        crop = list(map(int, crop))
+        tgt_img = img.crop(crop)
+        tgt_fname = f'{TRAIN_PATH}{tgt_img_id}.jpg'
+        tgt_img.save(tgt_fname)
+
+
+# In[46]:
+
+
+NUM_JOBS = 4
+MAX_CONCURRENT_JOBS = 1000
+WAIT_SECONDS = 1
+#df = df_bboxes[:30000]
 df = df_bboxes
 
 
-# In[28]:
+# In[47]:
 
 
 print("Converting %d bounding boxes" % len(df))
 tgt_df = pd.DataFrame(columns=['ImageID', 'LabelName'])
-prev_id = None
-for idx, row in tqdm(df.iterrows(), total=len(df)):
-    img_id = row['ImageID']
-    if img_id != prev_id:
-        fname = f'{DATA_PATH}train/{img_id}.jpg'
-        prev_id = img_id
-        img = PIL.Image.open(fname)
-        sub_idx = 0
-        sub_map = {}
-    w, h = img.size
-    bbox = round(w*row['XMin'], 0), round(h*row['YMin'], 0), round(w*row['XMax'], 0), round(h*row['YMax'], 0)
-    bbox = tuple(map(int, bbox))
-    bbox = (max(0, bbox[0]), max(0, bbox[1]), max(w, bbox[2]), max(h, bbox[3]))
-    tgt_img_id = sub_map.get(bbox)
-    if tgt_img_id is None:
-        tgt_img_id = f'{img_id}_{sub_idx:05}'
-        tgt_fname = f'{TRAIN_PATH}{tgt_img_id}.jpg'
-        tgt_img = img.crop(bbox)
-        tgt_img.save(tgt_fname)
-        sub_map[bbox] = tgt_img_id
-        sub_idx += 1
-    tgt_label = row['LabelName']
-    tgt_df = tgt_df.append({'ImageID': tgt_img_id, 'LabelName': tgt_label}, ignore_index=True)
-tgt_df.to_csv(f'{TGT_PATH}train_proc.csv', index=False)
+fs = []
+with futures.ThreadPoolExecutor(max_workers=NUM_JOBS) as executor:
+    job = None
+    for idx, row in tqdm(df.iterrows(), total=len(df)):
+        img_id = row['ImageID']
+        if job is None:
+            job = Job(img_id)
+        if not job.add_row(row):
+            fs.append(job.submit(executor))
+            tgt_df = job.add_labels(tgt_df)
+            job = Job(img_id)
+            job.add_row(row)
+            if len(fs) == MAX_CONCURRENT_JOBS:                
+                print("Have %d jobs to do, waiting for %d seconds" % (len(fs), WAIT_SECONDS))
+                _, fs = futures.wait(fs, timeout=WAIT_SECONDS)
+                fs = list(fs)
+    fs.append(job.submit(executor))
+    tgt_df = job.add_labels(tgt_df)
+    tgt_df.to_csv(f'{TGT_PATH}train_proc.csv', index=False)
+    print("Waiting for %d jobs to be completed" % len(fs))
+    futures.wait(fs)
 
